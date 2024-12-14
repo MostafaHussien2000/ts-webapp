f6b2fc73ca9c99cd0024639ac53ead5e0a29bc23
MostafaHussien2000 <mabozeed70@gmail.com>
2024-12-15 00:41:42 +0200

    creating out first decrator

---
+++ src/decorators.ts
@@ -0,0 +1,16 @@
+export const minValue =
+  (propName: string, min: number) =>
+  (
+    constructor: any,
+    methodName: string,
+    descriptor: PropertyDescriptor
+  ): any => {
+    const origFunction = descriptor.value;
+    descriptor.value = async function wrapper(...args) {
+      let results = await origFunction.apply(this, args);
+      return results.map((r) => ({
+        ...r,
+        [propName]: r[propName] < min ? min : r[propName],
+      }));
+    };
+  };
